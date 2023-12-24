726490178a999040489547b45a694f3f2af6f2fc
Nargiz Mamedova <nargizm7@yandex.ru>
2023-12-25 02:08:09 +0300

    [T-8] fixed main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { Callback, Context, Handler } from 'aws-lambda';
 import serverlessExpress from '@vendia/serverless-express';
 
 let server: Handler;
-const port = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,24 +13,18 @@ async function bootstrap() {
     origin: (req, callback) => callback(null, true),
   });
   app.use(helmet());
-
-  await app.listen(port);
-  // await app.init();
+  await app.init();
   
-  // const expressApp = app.getHttpAdapter().getInstance();
-  // return serverlessExpress({ app: expressApp });
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
 }
 
-bootstrap().then(() => {
-  console.log('App is running on %s port', port);
-});
-
-// export const handler: Handler = async (
-//   event: any,
-//   context: Context,
-//   callback: Callback,
-// ) => {
-//   server = server ?? (await bootstrap());
-//   console.log('App is running')
-//   return server(event, context, callback);
-// };
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
+  console.log('App is running')
+  return server(event, context, callback);
+};
