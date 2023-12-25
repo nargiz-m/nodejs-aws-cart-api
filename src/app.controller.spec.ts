0a9e33200a7c4fad39088096e2cbfc10b3e8ea39
Nargiz Mamedova <nargizm7@yandex.ru>
2023-12-26 02:38:38 +0300

    [T-8] fixed error

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AuthModule } from './auth/auth.module';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,7 +7,6 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      imports: [AuthModule],
       providers: [],
     }).compile();
 
